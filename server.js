e865456419dbbae2dd26a9c75f16127e136ed145
Fatima Basharat <fatima8746@gmail.com>
2023-08-23 16:18:43 -0400

    added footer

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const express = require('express');
 //const path = require('path');
 const app = express();
@@ -10,24 +9,34 @@ const session = require('express-session');
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const User = require('./models/userModel')
-
 const PORT = process.env.PORT || 3000
 
+
+//Use .env file in config folder
+require("dotenv").config();
+
+//Connect To Database
 connectDB()
 
+//Body Parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 //app.use(express.static(path.join(__dirname, 'public')));
+
+//Static Folder
 app.use(express.static('public'));
+
+//Using EJS for views
 app.set('view engine', 'ejs');
 
-
+//Setup Sessions - stored in MongoDB
 app.use(session({
   secret: 'this is catbook', 
   resave: false,
   saveUninitialized: false
 }))
 
+//Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
 
@@ -41,9 +50,11 @@ app.use((req,res,next) => {
   next()
 })
 
+//Setup Routes For Which The Server Is Listening
 app.use('/', catRoutes);
 app.use('/', userRoutes)
 
+//Server Running
 mongoose.connection.once('open', () => {
   console.log('Connected to MongoDB')
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
